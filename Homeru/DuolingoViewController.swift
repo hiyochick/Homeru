3e337dba60b68563b8651a13615edfe4eb04af73
HiyoriNagase <hiyorinmx@icloud.com>
2024-05-22 20:36:46 +0900

    一旦画面作成

---
+++ Homeru/DuolingoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DuolingoViewController.swift
+//  Homeru
+//
+//  Created by chick on 2024/05/22.
+//
+
+import UIKit
+
+class DuolingoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
