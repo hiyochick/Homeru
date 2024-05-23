e4c0f83080ecc10e43a1a73540aa7d405ab1bad5
HiyoriNagase <hiyorinmx@icloud.com>
2024-05-23 13:57:59 +0900

    Duolingo画面いけそう！！

--- Homeru/DuolingoViewController.swift
+++ Homeru/DuolingoViewController.swift
@@ -8,6 +8,28 @@
 import UIKit
 
 class DuolingoViewController: UIViewController {
+    
+    @IBOutlet var tango1: UILabel!
+    @IBOutlet var tango2: UILabel!
+    @IBOutlet var tango3: UILabel!
+    @IBOutlet var tango4: UILabel!
+    @IBOutlet var tango5: UILabel!
+    @IBOutlet var tango6: UILabel!
+    
+    @IBOutlet var tan1: UIButton!
+    @IBOutlet var tan2: UIButton!
+    @IBOutlet var tan3: UIButton!
+    @IBOutlet var tan4: UIButton!
+    @IBOutlet var tan5: UIButton!
+    @IBOutlet var tan6: UIButton!
+    @IBOutlet var tan7: UIButton!
+    @IBOutlet var tan8: UIButton!
+    @IBOutlet var tan9: UIButton!
+    @IBOutlet var tan10: UIButton!
+    @IBOutlet var tan11: UIButton!
+    @IBOutlet var tan12: UIButton!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,15 +37,11 @@ class DuolingoViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func reset () {
+    
     }
-    */
+    
+
+ 
 
 }
