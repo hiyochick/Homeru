69eda0927596ced48e8fb37ef432994fea6e07fd
HiyoriNagase <hiyorinmx@icloud.com>
2024-05-29 18:38:52 +0900

    チュートリアルのUIPageViewControllerがうまくいかない…

---
+++ Homeru/FifthViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FifthViewController.swift
+//  Homeru
+//
+//  Created by chick on 2024/05/29.
+//
+
+import UIKit
+
+class FifthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Homeru/HometaroViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HometaroViewController.swift
+//  Homeru
+//
+//  Created by chick on 2024/05/26.
+//
+
+import UIKit
+
+class HometaroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
